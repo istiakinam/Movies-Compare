bd75350072be5b7ab6a360625d512dfca17bfc58
istiakinam <istiak.inam@gmail.com>
2023-08-20 23:07:33 +0600

    displaying autocomplete

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,3 +1,15 @@
-it("works!", () => {
-
+it("Shows an autocomplete:", () => {
+    createAutoComplete({
+        root: document.querySelector("#target"),
+        fetchData() {
+            return [
+                { Title: "Silence"},
+                { Title: "Moderate"},
+                { Title: "Shout"}
+            ]
+        },
+        renderOption(movie) {
+            return movie.Title
+        }
+    })
 })
