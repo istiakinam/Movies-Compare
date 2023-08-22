1fad2a88853e79878a10392c66b642ae96005143
istiakinam <istiak.inam@gmail.com>
2023-08-22 06:54:48 +0600

    testing active dropdown

--- test/autocomplete.test.js
+++ test/autocomplete.test.js
@@ -1,4 +1,5 @@
-it("Shows an autocomplete:", () => {
+beforeEach( () => {
+    document.querySelector('#target').innerHTML = ''
     createAutoComplete({
         root: document.querySelector("#target"),
         fetchData() {
@@ -12,8 +13,20 @@ it("Shows an autocomplete:", () => {
             return movie.Title
         }
     })
+})
 
+it("Dropdown starts closed", () => {
     const dropdown = document.querySelector('.dropdown')
 
     expect(dropdown.className).not.to.include('is-active')
+})
+
+it("After searching, dropdown opens", () => {
+    const input = document.querySelector('input')
+    input = 'Circle'
+    input.dispatchEvent(new Event('input'))
+
+    const dropdown = document.querySelector('.dropdown')
+    
+    expect(dropdown.className).to.include('is-active')
 })
